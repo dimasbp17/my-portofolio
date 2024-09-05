c766fa5ddd1929094b254f7c4695a2966ffb6b51
dimasbp17 <dimas.prasetyo@students.amikom.ac.id>
2024-09-05 22:12:33 +0700

    update skill

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,30 +1,83 @@
 import React from 'react';
-import javascript from '../../public/images/javascript.png';
-import skill from '../data/skill';
 import { Link } from 'react-router-dom';
+import {
+  IoLogoCss3,
+  IoLogoHtml5,
+  IoLogoJavascript,
+  IoLogoNodejs,
+  IoLogoReact,
+} from 'react-icons/io5';
+import {
+  SiAdobeillustrator,
+  SiAdobephotoshop,
+  SiMysql,
+  SiNextdotjs,
+  SiTailwindcss,
+} from 'react-icons/si';
+import { FaBootstrap, FaFigma, FaGithub } from 'react-icons/fa6';
+import skills from '../data/skill.js';
 
 const Skill = () => {
+  const iconMap = {
+    IoLogoHtml5: <IoLogoHtml5 />,
+    IoLogoCss3: <IoLogoCss3 />,
+    IoLogoJavascript: <IoLogoJavascript />,
+    IoLogoReact: <IoLogoReact />,
+    SiNextdotjs: <SiNextdotjs />,
+    IoLogoNodejs: <IoLogoNodejs />,
+    SiMysql: <SiMysql />,
+    SiTailwindcss: <SiTailwindcss />,
+    FaBootstrap: <FaBootstrap />,
+    FaFigma: <FaFigma />,
+    FaGithub: <FaGithub />,
+    SiAdobeillustrator: <SiAdobeillustrator />,
+    SiAdobephotoshop: <SiAdobephotoshop />,
+  };
   return (
     <>
       <div className="flex flex-col items-center justify-center w-full min-h-screen p-5 text-white md:px-20">
-        <div className="flex flex-col items-start w-full mt-5 mb-10 text-4xl font-bold md:flex-row md:items-center text-nowrap">
+        <div className="flex flex-col items-start w-full mt-5 mb-5 text-4xl font-bold md:flex-row md:items-center text-nowrap">
           Kemampuan <span className="mx-2 text-hijau">&</span> Keahlian
           <hr className="w-full border border-white md:ml-5" />
         </div>
 
+        <div className="my-5">Teknologi</div>
         <div className="grid grid-cols-2 gap-2 md:gap-5 md:grid-cols-5">
-          {skill.map((skill) => (
-            <Link
-              to={skill.link}
-              className="text-center duration-300 hover:scale-105"
+          {skills.tech.map(({ skill, link, logo }, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center p-4 duration-300 bg-transparent border border-white rounded-lg size-32 hover:bg-hijau hover:text-white"
             >
-              <img
-                src={skill.image}
-                alt=""
-                className="size-32 md:size-36"
-              />
-              <span className="font-semibold">{skill.skill}</span>
-            </Link>
+              <div className="text-2xl">{iconMap[logo]}</div>
+              <Link
+                to={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-2 text-sm font-medium text-center hover:underline"
+              >
+                {skill}
+              </Link>
+            </div>
+          ))}
+        </div>
+
+        <div className="my-5">Alat</div>
+        <div className="grid grid-cols-2 gap-2 md:gap-5 md:grid-cols-5">
+          {skills.tools.map(({ skill, link, logo }, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center p-4 duration-300 bg-transparent border border-white rounded-lg size-32 hover:bg-hijau hover:text-white"
+            >
+              <div className="text-2xl">{iconMap[logo]}</div>
+              <Link
+                to={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-2 text-sm font-medium text-center hover:underline"
+              >
+                {skill}
+              </Link>
+            </div>
           ))}
         </div>
       </div>
