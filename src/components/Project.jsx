3be5670d24c3786ca9b1df3e8d78a53e031dc4e1
dimasbp17 <dimas.prasetyo@students.amikom.ac.id>
2024-09-17 13:32:52 +0700

    update:project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,7 @@
 import { Button } from '@material-tailwind/react';
 import React from 'react';
+import project from '../data/project';
+import { Link } from 'react-router-dom';
 
 const Project = () => {
   return (
@@ -19,30 +21,43 @@ const Project = () => {
             Project Saya
           </div>
         </div>
-        <div className="grid w-full grid-cols-1 md:grid-cols-3">
-          <div className="w-full p-5 bg-black border border-white rounded-xl">
-            <div className="flex flex-col w-full ">
-              <div className="w-full bg-white h-28"></div>
-              <div className="flex flex-col items-start">
-                <span className="text-xl font-semibold text-hijau">
-                  Safar Link
-                </span>
-                <span className="text-base text-justify text-white/90">
-                  Safar Link merupakan suatu aplikasi berbasis web yang
-                  digunakan untuk membuat suatu laman yang dapat di share kepada
-                  khalayak umum.
-                </span>
-              </div>
-              <div className="flex items-center justify-center mt-2">
-                <Button
-                  size="md"
-                  className="capitalize bg-hijau"
-                >
-                  Buka Project
-                </Button>
+        <div className="grid w-full grid-cols-1 gap-5 md:grid-cols-3">
+          {project.map((data) => (
+            <div
+              key={data.id}
+              className="w-full p-5 bg-black border border-white rounded-xl"
+            >
+              <div className="flex flex-col w-full ">
+                <div className="w-full ">
+                  <img
+                    src={data.image}
+                    alt={data.title}
+                  />
+                </div>
+                <div className="flex flex-col items-start gap-2">
+                  <span className="mt-2 text-xl font-semibold text-hijau">
+                    {data.title}
+                  </span>
+                  <span className="text-base text-justify text-white/90">
+                    {data.description}
+                  </span>
+                </div>
+                <div className="flex items-center justify-center mt-2">
+                  <Link
+                    to={data.href}
+                    target="blank"
+                  >
+                    <Button
+                      size="md"
+                      className="capitalize bg-hijau"
+                    >
+                      Buka Project
+                    </Button>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
